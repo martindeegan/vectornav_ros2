b5679bd200c7f3137700aa0dc4cc4d582a241ed9
Martin Deegan <mdeegan@umich.edu>
2020-03-14 17:54:20 -0400

    Initial commit

---
+++ cpp/build/CMakeFiles/libvncxx.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/attitude.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/attitude.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/compositedata.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/compositedata.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/conversions.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/conversions.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/criticalsection.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/criticalsection.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/dllvalidator.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/dllvalidator.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/error_detection.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/error_detection.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/event.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/event.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/ezasyncdata.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/ezasyncdata.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/memoryport.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/memoryport.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/packet.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/packet.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/packetfinder.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/packetfinder.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/port.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/port.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/position.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/position.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/searcher.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/searcher.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/sensors.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/sensors.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/serialport.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/serialport.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/thread.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/thread.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/types.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/types.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/util.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/util.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/utilities.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/utilities.cpp.o"
+  "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/src/vntime.cpp" "/home/martin/Downloads/vnproglib-1.1.5.0/cpp/build/CMakeFiles/libvncxx.dir/src/vntime.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
